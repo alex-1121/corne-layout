d92305c7824dd20311ca7fc5894ec26ec0f15d32
Aleksandr Sajenko <aleksandr.sajenko@forwardview.eu>
2021-09-12 20:41:56 +0300

    Change layer combination to activate ADJUST layer

--- keymap.c
+++ keymap.c
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //int RGB_current_mode;
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _NUM, _SYM, _ADJUST);
+    return update_tri_layer_state(state, _NUM, _MEDIA, _ADJUST);
 }
 
 // Setting ADJUST layer RGB back to default
