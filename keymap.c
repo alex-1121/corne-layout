0891c91fdf2bce9ddf8794bbc3ae5be059c65432
Aleksandr Sajenko <aleksandr.sajenko@forwardview.eu>
2021-09-12 20:36:57 +0300

    Add GUI MT to ESC, and move CTRL MT to Enter

--- keymap.c
+++ keymap.c
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      //,-----------------------------------------.                ,---------------------------------------------.
      KC_ALTTAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_BSPC,
      //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-  LCTL_T(KC_ESC),KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_ALTQUOT,
+  LGUI_T(KC_ESC),KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_ALTQUOT,
      //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-       KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_ENT,
+       KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,LCTL_T(KC_ENT),
      //|------+------+------+------+------+------+------|  |------+------+------+-------+------+-------+--------|
-                                       NUM, KC_LGUI, SYM,   KC_SPC,   NAV, MEDIA
+                                       NUM, KC_LGUI, SYM,   KC_SPC,   NAV, MEDIA;
                                  //`--------------------'  `--------------------'
   ),
 
