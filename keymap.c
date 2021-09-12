95d6b2765a6075c0fe536fc14fe2bf16907f94ea
Aleksandr Sajenko <aleksandr.sajenko@forwardview.eu>
2021-09-12 21:08:29 +0300

    Fix typo

--- keymap.c
+++ keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
        KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,LCTL_T(KC_ENT),
      //|------+------+------+------+------+------+------|  |------+------+------+-------+------+-------+--------|
-                                       NUM, KC_LGUI, SYM,   KC_SPC,   NAV, MEDIA;
+                                       NUM, KC_LGUI, SYM,   KC_SPC,   NAV, MEDIA
                                  //`--------------------'  `--------------------'
   ),
 
